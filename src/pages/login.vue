81b7d4e22a8b49e0d360c70ff875090f07c1cfb2
Rodolfo Roman <rromanv@gmail.com>
2021-10-18 01:14:41 -0500

    Finished

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import useAuth from "../composable/useAuth";
+import useError from "../composable/useError";
 
 const { isAuthenticated, login } = useAuth();
 
@@ -14,8 +15,17 @@ const logginIn = () => {
   login(username.value, password.value);
   if (isAuthenticated.value) {
     router.push("/");
+  } else {
+    setError("Invalid username or password");
+    start();
   }
 };
+
+const { error, setError } = useError();
+
+import { useTimeout, promiseTimeout } from "@vueuse/core";
+
+const { ready, start } = useTimeout(3000, { controls: true });
 </script>
 
 <template>
@@ -48,6 +58,12 @@ const logginIn = () => {
           Login
         </button>
       </form>
+    </div>
+    <div
+      v-if="!ready && error"
+      class="absolute w-1/3 p-4 text-center text-red-800 bg-red-300 rounded-lg  bottom-2 right-2"
+    >
+      {{ error }}
     </div>
   </div>
 </template>
